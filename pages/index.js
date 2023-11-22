f08aae069a02570a125f149dd4a35a61494cca4c
vorobevvs <woroboyvwalera@gmail.com>
2023-11-22 13:25:39 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import MainContainer from "./../components/MainContainer";
+
+const index = () => {
+  return (
+    <>
+      <MainContainer keywords={"main page"}>
+        <h1>Главная страница</h1>
+      </MainContainer>
+    </>
+  );
+};
+
+export default index;
